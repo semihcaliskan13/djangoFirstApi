fc6babee2dc398854aacaadac68856eba5ae3ecc
Semih <1030510071@erciyes.edu.tr>
2023-04-21 23:20:54 +0300

    yeni

--- semester/views.py
+++ semester/views.py
@@ -17,7 +17,7 @@ class SemesterList(generics.ListCreateAPIView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        # query_params içinden id ve name parametrelerini alın farklı easy
+        # query_params içinden id ve name parametrelerini alın farklı easy yeni commit denemesi
         id = self.request.query_params.get('id', None)
         # name = self.request.query_params.get('name', None)
 
