5a9ff6a0d052373cb476fbab1767061eeaa8b352
Semih <1030510071@erciyes.edu.tr>
2023-04-22 13:17:56 +0300

    urls

--- djangoFirstApi/urls.py
+++ djangoFirstApi/urls.py
@@ -64,14 +64,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('account/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('account/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger',
             cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc',
             cache_timeout=0), name='schema-redoc'),
-    path('semesters', include('semester.urls')),
-    path('courses', include('course.urls'))
+    path('semesters/', include('semester.urls')),
+    path('courses/', include('course.urls'))
 ]
